c2ec4a950165ac53de48339202118aa5d4365b9c
D01phiN90 <denisdreidt@gmail.com>
2021-08-26 06:29:45 +0300

    Initial commit

---
+++ src/com/company/TapeDeck.java
@@ -0,0 +1,11 @@
+package com.company;
+
+class TapeDeck {
+    boolean canRecord = false;
+    void playTape() {
+        System.out.println("пленка проигрывается");
+    }
+    void recordTape() {
+        System.out.println("идет запись на пленку");
+    }
+}
